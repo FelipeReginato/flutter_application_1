630084c2696303615badc1c9085b54d287e3cf3b
FelipeReginato <reginatofelipe16@outlook.com>
2023-04-15 17:31:28 -0300

    FirstCommit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar (
+          backgroundColor: Color.fromARGB(255, 34, 145, 7),
+          centerTitle: true,
+          title: Text('Meu Primeiro App')
+          ),
+          body: Column(
+            children: const [
+              Center(
+                child: Text(
+                  'Hello World',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color.fromARGB(255, 34, 145, 7)
+                  ),
+                  ),
+              )
+            ]
+          
+      
+          ),
+    )
+  )
+  );
+  
+}
